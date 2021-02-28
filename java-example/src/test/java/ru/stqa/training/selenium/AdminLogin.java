14c764ffdf602cddb624baeaa3f0db9ec12eb6b2
Astrakhantsev Vadim <vadymka04@gmail.com>
2021-02-28 21:16:36 +0300

    Задание 7

--- java-example/src/test/java/ru/stqa/training/selenium/AdminLogin.java
+++ java-example/src/test/java/ru/stqa/training/selenium/AdminLogin.java
@@ -1,13 +1,16 @@
 package ru.stqa.training.selenium;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AdminLogin {
@@ -15,10 +18,15 @@ public class AdminLogin {
     private WebDriverWait wait;
 
     @Before
-    public void start(){
+    public void start() {
         driver = new ChromeDriver();
+//        driver = new FirefoxDriver();
+//        DesiredCapabilities caps = new DesiredCapabilities();
+//        caps.setCapability(InternetExplorerDriver.REQUIRE_WINDOW_FOCUS, true);
+//        InternetExplorerOptions options = new InternetExplorerOptions().setPageLoadStrategy(PageLoadStrategy.NONE);
+//        driver = new InternetExplorerDriver(options);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 10);
+//        wait = new WebDriverWait(driver, 10);
     }
 
     @Test
@@ -27,10 +35,36 @@ public class AdminLogin {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
+
+        By header = By.xpath("//td[@id='content']/h1");
+
+//        WebElement boxMenu = driver.findElement(By.cssSelector("#box-apps-menu"));
+        List<WebElement> menuItems = driver.findElements(By.cssSelector("#box-apps-menu>#app-"));
+        int menuItemCount = menuItems.size();
+
+        for (int i = 0; i < menuItemCount; i++) {
+            menuItems.get(i).click();
+            String headerText = driver.findElement(header).getText();
+            Assert.assertTrue(headerText.length() > 0);
+            menuItems = driver.findElements(By.cssSelector("#box-apps-menu>#app-"));
+//            List<WebElement> menuSubItems = menuItems.get(i).findElements(By.cssSelector(".docs"));
+            List<WebElement> menuSubItems = driver.findElements(By.cssSelector("#box-apps-menu>#app->.docs>li")); //обновляем список - структура изменилась
+            if (menuSubItems.size()!=0){
+                for (int s = 0; s < menuSubItems.size(); s++){
+                    menuSubItems.get(s).click();
+                    headerText = driver.findElement(header).getText();
+                    Assert.assertTrue(headerText.length() > 0);
+                    menuSubItems = driver.findElements(By.cssSelector("#box-apps-menu>#app->.docs>li"));//обновляем список - структура изменилась
+                    menuItems = driver.findElements(By.cssSelector("#box-apps-menu>#app-"));//обновляем список - структура изменилась
+                }
+            }
+        }
+
+
     }
 
     @After
-    public void stop(){
+    public void stop() {
         driver.quit();
         driver = null;
     }
